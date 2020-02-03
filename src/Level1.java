9588492a9049534ff25f5fb2f3e9cf9eabe12f11
PavelSOS <sawel@yandex.ru>
2020-02-03 20:31:53 +0800

    Revert "initial commit"

--- src/Level1.java
+++ src/Level1.java
@@ -2,60 +2,58 @@ import java.util.*;
 
 public class Level1 {
     public static String mainStr = "";
-    public static List<String> mainStrMod = new ArrayList<>();
-    public static int indexMod = 1;
+    public static List<String> mainStrMod = new ArrayList<>(Arrays.asList(""));
+    public static int indexMod = 0;
     public static int countUndo = 0;
 
     public static String BastShoe(String command) {
         String[] resArr = command.split(" ", 2);
         int mSLenth = mainStr.length();
-        int x = Integer.parseInt(resArr[0]);
-        switch (x) {
+        switch (Integer.parseInt(resArr[0])) {
             case 1:
                 if (countUndo > 0) {
                     mainStrMod.clear();
                     mainStrMod.add(mainStr);
                     countUndo = 0;
-                    indexMod = 1;
+                    indexMod = 0;
                 }
                 mainStr = mainStr + resArr[1];
                 mainStrMod.add(mainStr);
+                indexMod++;
                 break;
-
             case 2:
                 int iEnd = Integer.parseInt(resArr[1]) > mSLenth ? 0 : mSLenth - Integer.parseInt(resArr[1]);
                 if (countUndo > 0) {
                     mainStrMod.clear();
                     mainStrMod.add(mainStr);
                     countUndo = 0;
-                    indexMod = 1;
+                    indexMod = 0;
                 }
                 mainStr = mainStr.substring(0, iEnd);
                 mainStrMod.add(mainStr);
+                indexMod++;
                 break;
-
             case 3:
                 try {
                     mainStr = Character.toString(mainStr.charAt(Integer.parseInt(resArr[1])));
-                    break;
                 } catch (Exception e) {
                     mainStr = "";
-                    break;
                 }
-
+                break;
             case 4:
-                indexMod++;
-                try {
-                    mainStr = mainStrMod.get(mainStrMod.size() - indexMod);
-                } catch (Exception e) {
-                    mainStr = mainStrMod.get(0);
+                indexMod--;
+                if (indexMod < 0) {
+                    indexMod = 0;
                 }
+                mainStr = mainStrMod.get(indexMod);
                 countUndo++;
                 break;
             case 5:
-                indexMod--;
-                if (indexMod < 1) indexMod = 1;
-                mainStr = mainStrMod.get(mainStrMod.size() - indexMod);
+                indexMod++;
+                if (indexMod > (mainStrMod.size() - 1)) {
+                    indexMod = mainStrMod.size() - 1;
+                }
+                mainStr = mainStrMod.get(indexMod);
                 break;
             default:
                 return mainStr;
